b33633c22c7a981422a7015447615860986caee5
B64283 <mattdarke@fullsail.edu>
2013-07-16 20:38:55 -0700

    commit

---
+++ MIU_Darke_Matthew/week2/GoldApp/js/json.js
@@ -0,0 +1,40 @@
+//Matthew Darke
+//VFW 1306
+//Project 4 Json
+
+var json = {
+	"vacation1": {
+		"group": ["group:", "Vacation"],		
+		"dest": ["Destination:", "Rome"],
+		"days": ["Days of travel:", "9"],
+		"clothes": ["checkbox Clothes:", "Clothes" ],
+		"meds":  ["checkbox Medication:", "Medication"],
+		"toiletries": ["checkbox Toiletries:", "Toiletries"],
+		"budget": ["Budget scale:", "5050"],
+		"date": ["Vacation starts on:", "July 4 2013"],
+		"extraNotes": ["extra thoughts!:", "Rome, here I come!"]	
+		},
+   "vacation2": {
+		"group": ["group:", "Busniess"],		
+		"dest": ["Destination:", "Israel"],
+		"days": ["Days of travel:", "9"],
+		"clothes": ["checkbox Clothes:", "Clothes" ],
+		"meds":  ["checkbox Medication:", "Medication"],
+		"toiletries": ["checkbox Toiletries:", "Toiletries"],
+		"budget": ["Budget scale:", "5050"],
+		"date": ["Vacation starts on:", "July 4 2013"],
+		"extraNotes": ["extra thoughts!:", "Israel, here I come!"]	
+   },
+    "vacation3": {
+		"group": ["group:", "Commute"],		
+		"dest": ["Destination:", "Israel"],
+		"days": ["Days of travel:", "9"],
+		"clothes": ["checkbox Clothes:", "Clothes" ],
+		"meds":  ["checkbox Medication:", "Medication"],
+		"toiletries": ["checkbox Toiletries:", "Toiletries"],
+		"budget": ["Budget scale:", "5050"],
+		"date": ["Vacation starts on:", "July 4 2013"],
+		"extraNotes": ["extra thoughts!:", "Israel, here I come!"]	
+   }
+
+}   
